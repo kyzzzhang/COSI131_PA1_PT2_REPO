7b9b3ced71f387781185feecfdaac9ffc4c81ae1
NinaXu317 <c1xu@brandeis.edu>
2018-10-08 19:41:44 -0400

    comment

--- src/cs131/pa1/filter/concurrent/ConcurrentREPL.java
+++ src/cs131/pa1/filter/concurrent/ConcurrentREPL.java
@@ -15,62 +15,65 @@ public class ConcurrentREPL {
 		currentWorkingDirectory = System.getProperty("user.dir");
 		Scanner s = new Scanner(System.in);
 		System.out.print(Message.WELCOME);
+		// list of thread
 		LinkedList<Thread> threads = new LinkedList<Thread>();
 		String command;
+		// set that contains alive threads
 		Set<String> aliveThread = new HashSet<String>();
 		while(true) {
 			//obtaining the command from the user
 			System.out.print(Message.NEWCOMMAND);
 			command = s.nextLine();
-			boolean containKillCommand = false;	
+			// split the command by space
 			String[] commandList = command.split("\\s+");
-			if (command.contains("kill")) {
-				int killAtIndex=-1;
-				for(int i = 0; i<commandList.length;i++){
-					if(commandList[i].equals("kill")){
-						containKillCommand = true;
-						killAtIndex = i;
-					}
-				}
-				if(containKillCommand){
-					kill(killAtIndex, commandList, threads);
-				} else {
-					otherCommands(command, commandList, threads);
-				}
+			// if the command is a kill command
+			if (command.startsWith("kill")) {
+				// run kill command
+				kill(commandList, threads);
+			// if the command is an exit command
 			} else if(command.equals("exit")) {
 				break;
+			// if the command is repl_jobs
 			} else if (command.trim().equals("repl_jobs")) {
-				int number = 1;
+				// number the thread
+				int threadNumber = 1;
+				// loop through all threads in the thread list
 				for (Thread t: threads) {
+					// check if the thread is alive
 					if (t.isAlive()) {
+						// check if the thread is alive and hasn't been printed out before
 						if (!aliveThread.contains(t.getName())) {
-							System.out.println("\t" + number + ". " + t.getName() + "&");
+							System.out.println("\t" + threadNumber + ". " + t.getName() + "&");
 						}
+						// add the thread to the alive thread set 
 						aliveThread.add(t.getName());
 					}
-					number++;
+					// adding thread number
+					threadNumber++;
 				}
 			} else {
+				// method for running other commands
 				otherCommands(command, commandList, threads);
 			}
 		}
+		// close the console
 		s.close();
 		System.out.print(Message.GOODBYE);
 	}
 
-	public static void kill(int killAtIndex, String[] commandList, LinkedList<Thread> threads) {
-		if(killAtIndex+1>=commandList.length){
+	public static void kill(String[] commandList, LinkedList<Thread> threads) {
+		if(commandList.length<2){
 			System.out.printf(Message.REQUIRES_PARAMETER.toString(), "kill");
 		} else {
 			int killNumber = 0;
 			try {
-				killNumber = Integer.parseInt(commandList[killAtIndex+1]);
+				killNumber = Integer.parseInt(commandList[1]);
 			} catch (NumberFormatException e) {
-				System.out.printf(Message.INVALID_PARAMETER.toString(), "kill "+commandList[killAtIndex+1]);
+				System.out.printf(Message.INVALID_PARAMETER.toString(), "kill "+commandList[1]);
 			}
 			if(killNumber !=0 ) {
 				if((killNumber)>threads.size()) {
-					System.out.printf(Message.INVALID_PARAMETER.toString(), "kill "+commandList[killAtIndex+1]);
+					System.out.printf(Message.INVALID_PARAMETER.toString(), "kill "+commandList[1]);
 				} else {
 					int killed = killNumber-1;
 					if (threads.get(killed).isAlive()) {
